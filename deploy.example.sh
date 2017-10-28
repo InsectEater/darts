2d3a9d08bdfd4172809bfcadda1d2e3f3bcf2b38
home <InsectEater@gmail.com>
2017-10-28 09:39:31 +0300

    Example deploy with git-ftp

---
+++ deploy.example.sh
@@ -0,0 +1,4 @@
+#git ftp init --user USERNAME --passwd PASSWORD ftp://YOUR-FTP-SERVER-ADDRESS/path/to/website/
+#git ftp push --user USERNAME --passwd PASSWORD ftp://YOUR-FTP-SERVER-ADDRESS/path/to/website/
+
+
